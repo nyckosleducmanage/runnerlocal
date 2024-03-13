cddafc4e147044df91cd8ae6f08ebf30719c055f
nyckosleducmanage <104419256+nyckosleducmanage@users.noreply.github.com>
2024-03-13 18:34:17 +0100

    Update nanoleaf.py

--- tools/nanoleaf.py
+++ tools/nanoleaf.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from nanoleafapi import discovery, Nanoleaf
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./tools')
 
 # Fonction pour obtenir l'adresse IP du Nanoleaf
 def get_nanoleaf_ip():
